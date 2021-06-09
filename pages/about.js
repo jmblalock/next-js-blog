85b27a363c2f2df3d7201e3ed328e7ebfc4b26ef
jmblalock <justin.m.blalock@gmail.com>
2021-06-08 20:08:17 -0500

    about page

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h2>Hi my name is</h2>
+      <h3>Justin Blalock.</h3>
+      <h3>I build things for the web.</h3>
+    </div>
+  );
+}
