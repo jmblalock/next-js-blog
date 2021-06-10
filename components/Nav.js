7701e624014902300248efbde1792098f5187943
jmblalock <justin.m.blalock@gmail.com>
2021-06-10 18:56:42 -0500

    nav work

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <div>
-    <p>Hello World!</p>
-  </div>
+  return (
+    <div class="min-h-full">
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/experience">Experience</Link>
+      <Link href="/work">Work</Link>
+      <Link href="/contact">Contact</Link>
+    </div>
+  );
 }
---
+++ pages/work.js
@@ -0,0 +1,3 @@
+export default function Workxperience() {
+  return <div>Work Page</div>;
+}
