60a0630207244e978d11bc3d282882ca4638f86e
jmblalock <justin.m.blalock@gmail.com>
2021-07-07 16:10:03 -0500

    styling work

--- pages/index.js
+++ pages/index.js
@@ -8,32 +8,31 @@ import Contact from "./contact";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-green-cyan text-center text-4xl m-36 font-mono">
+    <div className="m-36 max-w-sm mx-auto">
+      <h1 className="text-green-cyan text-4xl font-mono">
         <Typing
           strings={[
-            "Hi, my name is",
-            "Justin Blalock.",
-            "I design and code things for the web.",
+            "Hi, my name is^1000\n`Justin Blalock.`^1000\n`I design and code things for the web.`",
           ]}
         />
       </h1>
-      <div className="m-36 mb-12 text-xl text-center text-gray-400">
+      <p className="text-xl text-gray-400">
         I'm an Austin-based software engineer who specializes in designing and
         bulding digital experiences. Currently, I am seeking a full-time
         position as a front-end developer.
-      </div>
-      <div className="flex justify-items-centerr">
+      </p>
+      <div className="flex">
         <Link href="mailto:justin.m.blalock@gmail.com">
-          <a className="mt-0 m-36 px-4 py-4 leading-none border rounded text-carolina-blue border-orange-pantone hover:text-orange-pantone flex-auto">
+          <a className="px-4 py-4 leading-none border rounded text-carolina-blue border-orange-pantone hover:text-orange-pantone">
             Get In Touch
           </a>
         </Link>
       </div>
-      <About />
+
+      {/* <About />
       <Experience />
       <Work />
-      <Contact />
-    </>
+      <Contact /> */}
+    </div>
   );
 }
