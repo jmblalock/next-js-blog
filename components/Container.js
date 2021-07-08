7450a71117a87be802cea350d5036e066a35bfe0
jmblalock <justin.m.blalock@gmail.com>
2021-07-07 20:11:49 -0500

    working on container component

--- components/Container.js
+++ components/Container.js
@@ -1,3 +1,7 @@
-export default function Container() {
-  return <div></div>;
+export default function Container({ children }) {
+  return (
+    <div className="container mx-auto py-12 px-6 xsm:px-8 md:px-12 md:py-24">
+      {children}
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Typing from "../components/Typing";
 import Link from "next/link";
+import Container from "../components/Container";
 import Triangle from "../components/Triangle";
 import About from "./about";
 import Experience from "./experience";
@@ -8,33 +9,36 @@ import Contact from "./contact";
 
 export default function Home() {
   return (
-    <div className="m-36">
-      <h1 className="py-4 text-green-cyan text-4xl font-mono">
-        <Typing
-          strings={[
-            "Hi, my name is...",
-            "Justin Blalock.",
-            "I design and code things for the web.",
-          ]}
-        />
-      </h1>
-      <p className="py-4 text-xl text-gray-400">
-        I'm an Austin-based software engineer who specializes in designing and
-        bulding digital experiences. Currently, I am seeking a full-time
-        position as a front-end developer.
-      </p>
-      <div className="flex py-4">
-        <Link href="mailto:justin.m.blalock@gmail.com">
-          <a className="px-4 py-4 leading-none border rounded text-carolina-blue border-orange-pantone hover:text-orange-pantone">
-            Get In Touch
-          </a>
-        </Link>
-      </div>
+    <>
+      <Container>Hello</Container>
+      <div className="m-36">
+        <h1 className="py-4 text-green-cyan text-4xl font-mono">
+          <Typing
+            strings={[
+              "Hi, my name is...",
+              "Justin Blalock.",
+              "I design and code things for the web.",
+            ]}
+          />
+        </h1>
+        <p className="py-4 text-xl text-gray-400">
+          I'm an Austin-based software engineer who specializes in designing and
+          bulding digital experiences. Currently, I am seeking a full-time
+          position as a front-end developer.
+        </p>
+        <div className="flex py-4">
+          <Link href="mailto:justin.m.blalock@gmail.com">
+            <a className="px-4 py-4 leading-none border rounded text-carolina-blue border-orange-pantone hover:text-orange-pantone">
+              Get In Touch
+            </a>
+          </Link>
+        </div>
 
-      {/* <About />
+        {/* <About />
       <Experience />
       <Work />
       <Contact /> */}
-    </div>
+      </div>
+    </>
   );
 }
