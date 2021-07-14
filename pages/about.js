6eae8f7bfac3cdca1f5980bfd207ed77ebc376de
jmblalock <justin.m.blalock@gmail.com>
2021-07-13 22:38:19 -0500

    pic work

--- pages/about.js
+++ pages/about.js
@@ -5,32 +5,36 @@ export default function About() {
   return (
     <Container>
       <h2 className="text-green-cyan text-4xl font-mono">About Me</h2>
-      <p className="pt-12 leading-7 tracking-wider text-2xl text-gray-400">
-        Hello! My name is Justin and I enjoy designing and creating things that
-        live on the internet. My interest in development started back in 2017
-        when I participated in my first{" "}
-        <a
-          className="text-carolina-blue hover:underline"
-          rel="noopener noreferrer"
-          href="https://blog.box.com/3-box-skills-built-our-latest-hackathon"
-          target="_blank"
-        >
-          hackathon
-        </a>{" "}
-        - coincidentally, I ended up learning a great deal about{" "}
-        <CodeTag>JavaScript</CodeTag>, <CodeTag>HTML</CodeTag> &{" "}
-        <CodeTag>CSS</CodeTag> by helping build a new project from scratch in a
-        short amount of time.
-      </p>
-      <picture className="">
-        <img
-          width="300"
-          height="300"
-          className="rounded-full"
-          src="/avatar.jpeg"
-          alt="My Avatar"
-        />
-      </picture>
+      <div className="grid grid-cols-4">
+        <p className="col-span-3 pt-12 leading-7 tracking-wider text-2xl text-gray-400">
+          Hello! My name is Justin and I enjoy designing and creating things
+          that live on the internet. My interest in development started back in
+          2017 when I participated in my first{" "}
+          <a
+            className="text-carolina-blue hover:underline"
+            rel="noopener noreferrer"
+            href="https://blog.box.com/3-box-skills-built-our-latest-hackathon"
+            target="_blank"
+          >
+            hackathon
+          </a>{" "}
+          - coincidentally, I ended up learning a great deal about{" "}
+          <CodeTag>JavaScript</CodeTag>, <CodeTag>HTML</CodeTag> &{" "}
+          <CodeTag>CSS</CodeTag> by working on a team developing a new project
+          from scratch in a short amount of time.
+        </p>
+        <picture className="">
+          <img
+            className="rounded-full border border-transparent hover:ring-4 hover:ring-dark-blue-grey"
+            src="/avatar.jpeg"
+            alt="My Avatar"
+          />
+        </picture>
+        <p className="col-start-2 col-span-3 pt-12 leading-7 tracking-wider text-2xl text-gray-400">
+          When I'm not coding, I enjoy spending time with my beautiful wife and
+          our two dogs, Remy and Quincy.
+        </p>
+      </div>
     </Container>
   );
 }
