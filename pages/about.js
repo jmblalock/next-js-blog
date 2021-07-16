f9dd6321ddb19f69a4b531ee0d36f7b036e625b4
jmblalock <justin.m.blalock@gmail.com>
2021-07-16 16:11:26 -0500

    about page z-index work

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ export default function About() {
     <Container>
       <h2 className="text-green-cyan text-4xl font-mono">About Me</h2>
       <div className="grid gap-8 md:grid-cols-4 ">
-        <p className="order-1 md:order-1 md:self-center md:col-span-3 tracking-wider text-xl text-gray-400">
+        <p className="order-1 md:order-1 md:self-center md:col-span-3 pt-12 tracking-wider text-xl text-gray-400">
           Hello! My name is Justin and I enjoy designing and creating things
           that live on the internet. My interest in development started back in
           2017 when I participated in my first{" "}
@@ -23,23 +23,26 @@ export default function About() {
           <CodeTag>CSS</CodeTag> by working on a team developing a new project
           from scratch in a short amount of time.
         </p>
-        <picture className="order-2 md:order-2">
-          <img
-            className="rounded-full border border-transparent hover:ring-4 hover:ring-dark-blue-grey"
-            src="/images/avatar.jpeg"
-            alt="My Avatar"
-          />
-        </picture>
+        <div className="z-0 w-full wrapper order-2 md:order-2">
+          <picture className="">
+            <img
+              className="z-10 w-full rounded-full border border-transparent hover:ring-4 hover:ring-dark-blue-grey"
+              src="/images/avatar.jpeg"
+              alt="My Avatar"
+            />
+          </picture>
+        </div>
         <picture className="order-4 md:order-3">
           <img
             className="rounded-full border border-transparent hover:ring-4 hover:ring-dark-blue-grey"
             src="/images/remy.jpeg"
-            alt="Remy Boy"
+            alt="Remy"
           />
         </picture>
         <p className="order-3 md:order-4 md:self-center md:col-span-3 tracking-wider text-xl text-gray-400">
-          When I'm not coding, I enjoy biking, snowboarding and adventuring with
-          my beautiful wife and our two troublemakers, Remy and Quincy.
+          When I'm not coding, I enjoy traveling, biking, snowboarding and
+          adventuring with my beautiful wife and our two troublemakers, Remy and
+          Quincy.
         </p>
         <div className="order-5 md:order-5 md:self-center md:col-span-3 tracking-wider text-xl text-gray-400">
           Here are a few technologies I've been working with recently:
@@ -68,7 +71,7 @@ export default function About() {
           <img
             className="rounded-full border border-transparent hover:ring-4 hover:ring-dark-blue-grey"
             src="/images/quincy.jpeg"
-            alt="Quincy Girl"
+            alt="Quincy"
           />
         </picture>
       </div>
